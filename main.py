4c7b27f777efbae507a25f2c59155a16eeb3bb65
Imamul Kabir Anan <anan.imamul@gmail.com>
2026-01-24 00:44:48 +0600

    upload

--- main.py
+++ main.py
@@ -1,35 +1,89 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Optional, List, Literal
 import json
 
 app = FastAPI()
+
+class Patient(BaseModel):
+
+    
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in mtrs')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi= round(self.weight / (self.height ** 2), 2)
+        return bmi
+    
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif 18.5 <= self.bmi < 24.9:
+            return 'Normal weight'
+        elif 25 <= self.bmi < 29.9:
+            return 'Overweight'
+        else:
+            return 'Obesity'
+
+
 
 def load_data():
     with open("patients.json", "r") as f:
         data = json.load(f)
     return data
-    
+
 
 @app.get("/")
 def hello():
-    return {"message": "Patient Management System API"}
+    return {'message':'Patient Management System API'}
 
-
-@app.get("/about")
+@app.get('/about')
 def about():
-    return {"message": "A fully functional Patient Management System API built with FastAPI."}
+    return {'message': 'A fully functional API to manage your patient records'}
 
-
-@app.get("/view")
+@app.get('/view')
 def view():
     data = load_data()
 
     return data
 
-
-@app.get("/patient/{patient_id}")
-def view_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve", example="P001")):
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P001')):
+    # load all the patients
     data = load_data()
 
     if patient_id in data:
         return data[patient_id]
-    raise HTTPException(status_code=404, detail="Patient not found")
+    raise HTTPException(status_code=404, detail='Patient not found')
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+
+
+
