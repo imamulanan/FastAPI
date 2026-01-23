31c123a8ca20304512409f7fb321b48513321ad1
Imamul Kabir Anan <anan.imamul@gmail.com>
2026-01-23 11:33:14 +0600

    upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Path, HTTPException
+import json
+
+app = FastAPI()
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+    
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Management System API"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "A fully functional Patient Management System API built with FastAPI."}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id: str = Path(..., description="The ID of the patient to retrieve", example="P001")):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="Patient not found")
